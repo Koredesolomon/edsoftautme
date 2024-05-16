7b54a4271e14d4cbcc3ebd4e4aec186cb91db06e
Korede <105435382+Koredesolomon@users.noreply.github.com>
2024-05-16 03:29:54 +0100

    Dashboard changes

--- components/Button.js
+++ components/Button.js
@@ -1,1 +1,42 @@
 //create all your buttons here
+
+import React from "react"
+import { TouchableOpacity, Image, StyleSheet, Text } from "react-native"
+import colors from "../components/constants/colors"
+import { useFonts } from "expo-font"
+
+
+const styles = StyleSheet.create({
+    button: {
+      backgroundColor: colors.blue,
+      alignItems: 'center',
+      width: 225,
+      paddingTop: 14,
+      paddingBottom: 14,
+      borderRadius: 10,
+    },
+    buttonText: {
+      fontSize: 16,
+      color: colors.white,
+      fontFamily: 'Mulish-SemiBold',
+    },
+  })
+
+
+export default Button = ({text}) => {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Mulish-SemiBold': require('../assets/fonts/Mulish-SemiBold.ttf'),
+
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+    return (
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>{text}</Text>
+        </TouchableOpacity>
+      )
+}
